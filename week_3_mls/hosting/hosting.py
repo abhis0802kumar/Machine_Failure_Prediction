b29ca6a2db2b674768c00aee1655ec4fa6398d1b
abhis0802kumar <abhis0802kumar@gmail.com>
2026-06-14 08:14:36 +0530

    Update hosting.py

--- week_3_mls/hosting/hosting.py
+++ week_3_mls/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="week_3_mls/deployment",     # the local folder containing your files
-    repo_id="abhis0802kumar/Machine-Failure-Prediction",          # the target repo
+    repo_id="abhis0802/Machine-Failure-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
